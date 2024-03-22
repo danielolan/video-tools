cd8d846b133bf6a024f3be08238fe8345480c0f0
daniel olano <daolano58@ucatolica.edu.co>
2024-03-21 19:35:39 -0500

    slidebar functional

--- src/components/shared/layouts/DashBoardLayout.tsx
+++ src/components/shared/layouts/DashBoardLayout.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import TopBar from "../../Videos/TopBar";
 import Sidebar from "../Sidebar";
+import { useRef } from "react";
 
 interface PropsLayoutDash {
   children: JSX.Element | JSX.Element[];
 }
 const DashBoardLayout: React.FC<PropsLayoutDash> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+ 
+  const sidebarRef = useRef();  // Agrega esta referencia
   
   const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
   useEffect(() => {
@@ -21,8 +24,19 @@ const DashBoardLayout: React.FC<PropsLayoutDash> = ({ children }) => {
 
   // Nuevo efecto para manejar el clic fuera del sidebar
   useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024 && sidebarOpen) {
+        setSidebarOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [sidebarOpen]);
+
+  useEffect(() => {
     const handleOutsideClick = (event) => {
-      if (sidebarOpen && !event.target.closest('.sidebar-class')) {
+      if (sidebarOpen && sidebarRef.current && !sidebarRef.current.contains(event.target)) {
         setSidebarOpen(false);
       }
     };
@@ -31,25 +45,11 @@ const DashBoardLayout: React.FC<PropsLayoutDash> = ({ children }) => {
     return () => document.removeEventListener('mousedown', handleOutsideClick);
   }, [sidebarOpen]);
 
-
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 1024 && sidebarOpen) {
-        setSidebarOpen(false);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [sidebarOpen]);
-
   return (
     <div className="flex h-screen p-5">
-      <Sidebar isOpen={sidebarOpen} className="sidebar-class"/>
+       <div ref={sidebarRef} className="sidebar-class">
+        <Sidebar isOpen={sidebarOpen} />
+      </div>
       <div className="flex-1 flex flex-col bg-white ml-4 rounded-2xl shadow px-8 w-full">
         <TopBar toggleSidebar={toggleSidebar} />
         {children}
